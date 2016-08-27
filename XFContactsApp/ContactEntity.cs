5579e7cd2efe6a9d635341bc72c13938c049c216
Rohit Mathews <rohitvipin@gmail.com>
2016-08-27 13:24:26 +0530

    * ContactEntity.cs: adding contact list
    * XFContactsApp.csproj:
    * XFContactsAppPage.xaml:
    * XFContactsAppPage.xaml.cs:

---
+++ XFContactsApp/ContactEntity.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+namespace XFContactsApp
+{
+    public class ContactEntity
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        public ImageSource Image => ImageSource.FromFile("user3");
+    }
+}
--- XFContactsApp/XFContactsAppPage.xaml.cs
+++ XFContactsApp/XFContactsAppPage.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using Xamarin.Forms;
+using System.Collections.Generic;
+using System;
+using System.Linq;
 
 namespace XFContactsApp
 {
@@ -7,9 +10,11 @@ namespace XFContactsApp
         public XFContactsAppPage()
         {
             InitializeComponent();
+
+            contactList.ItemsSource = GetContacts();
         }
 
-        async void Button_Clicked(object sender, System.EventArgs e)
+        async void Button_Clicked(object sender, EventArgs e)
         {
             var clickedButton = sender as Button;
 
@@ -26,6 +31,58 @@ namespace XFContactsApp
             }
 
             await DisplayAlert("Clicked", $"You have clicked {selectedContact}", "Ok");
+        }
+
+        void SearchBar_ButtonPressed(object sender, EventArgs e)
+        {
+            var searchBar = sender as SearchBar;
+
+            if (searchBar == null)
+            {
+                return;
+            }
+
+            contactList.ItemsSource = string.IsNullOrWhiteSpace(searchBar.Text) 
+                                            ? GetContacts() : GetFilteredContacts(searchBar.Text);
+        }
+
+        void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var searchBar = sender as SearchBar;
+
+            if (searchBar == null)
+            {
+                return;
+            }
+
+            contactList.ItemsSource = string.IsNullOrWhiteSpace(searchBar.Text)
+                                            ? GetContacts() : GetFilteredContacts(searchBar.Text);
+        }
+
+        static IEnumerable<ContactEntity> GetContacts()
+        {
+            var contacts = new List<ContactEntity>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                contacts.Add(new ContactEntity
+                {
+                    Name = $"Contact Name {i}",
+                    Description = $"Description {i}"
+                });
+            }
+
+            return contacts;
+        }
+
+        IEnumerable<ContactEntity> GetFilteredContacts(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return null;
+            }
+
+            return contactList?.ItemsSource?.Cast<ContactEntity>()?.Where(x => x.Name.Contains(text));
         }
     }
 }
