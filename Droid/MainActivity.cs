0c2cd10e48008dd6be744c0734c1530a000aceb4
Rohit Mathews <rohitvipin@gmail.com>
2016-08-28 00:05:28 +0530

    theme

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,18 +1,23 @@
 ﻿
 using Android.App;
 using Android.Content.PM;
+using Android.Graphics;
 using Android.OS;
+using Android.Views;
 using Xamarin.Forms.Platform.Android;
 
 namespace XFContactsApp.Droid
 {
-    [Activity(Label = "XFContactsApp.Droid", Icon = "@drawable/icon", Theme="@style/MyCustomTheme"
+    [Activity(Label = "XFContactsApp.Droid", Icon = "@drawable/icon", Theme = "@style/MyCustomTheme"
               , MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : FormsApplicationActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
+            ActionBar.SetIcon(Color.Transparent);
+            Window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
 
             Xamarin.Forms.Forms.Init(this, savedInstanceState);
 
